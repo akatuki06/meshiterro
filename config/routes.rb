af430759b82cc2e19c5407f4acda71d34428da10
akatuki06 <akatuki0623@gmail.com>
2018-02-13 17:42:26 +0900

    Finish 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  # get 'users/show'
+
   devise_for :users
 
   root'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 
 # get 画面変わります
 # post データ送信します
